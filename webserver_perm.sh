bca698179e62174568f6453317e8d063fcc97f60
Ebuk-a <cakwiwuuzoma@gmail.com>
2023-05-30 16:16:11 +0100

    remove interative from docker exec

--- webserver_perm.sh
+++ webserver_perm.sh
@@ -2,4 +2,4 @@
 
 sleep 60
 WEBSERVER_ID=`docker ps | grep webserver | awk '{print $1}'`
-docker exec -u root -i -t $WEBSERVER_ID chmod 777 /opt/airflow
+docker exec -u root $WEBSERVER_ID chmod 777 /opt/airflow
