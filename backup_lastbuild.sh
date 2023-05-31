a517c7d6f84714a3000a641fbd2c66468e844623
Ebuk-a <cakwiwuuzoma@gmail.com>
2023-05-31 09:12:10 +0100

    update docker creds

--- backup_lastbuild.sh
+++ backup_lastbuild.sh
@@ -13,3 +13,20 @@ else
         rm -rf airflow-ci-jenkins@tmp
         mv /var/lib/jenkins/workspace/airflow-ci-jenkins /var/lib/jenkins/workspace/airflow-ci-jenkins_$BUILD_NUMBER_$BUILD_TIMESTAMP
 fi
+
+
+
+#!/bin/bash
+echo "###############################################################"
+ls /var/lib/jenkins/workspace/airflow-ci-jenkins &> /dev/null
+while [ $? -eq 0 ]
+do
+    echo "Backing up the old build file as airflow-ci-jenkins_v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+    rm -rf airflow-ci-jenkins@tmp
+    mv /var/lib/jenkins/workspace/airflow-ci-jenkins /var/lib/jenkins/workspace/airflow-ci-jenkins_v$BUILD_NUMBER_$BUILD_TIMESTAMP
+    ls /var/lib/jenkins/workspace/airflow-ci-jenkins &> /dev/null
+    sleep 2
+done
+echo "Backed up to airflow-ci-jenkins_v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
+echo "###############################################################"
