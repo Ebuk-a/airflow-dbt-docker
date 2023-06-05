00da32a2a748bf9bea686be7507a570af6669854
Ebuk-a <cakwiwuuzoma@gmail.com>
2023-06-05 08:46:12 +0100

    update secure permission on websever container

--- webserver_perm.sh
+++ webserver_perm.sh
@@ -2,5 +2,6 @@
 
 sleep 60
 WEBSERVER_ID=`docker ps | grep webserver | awk '{print $1}'`
-docker exec -u root $WEBSERVER_ID chmod 777 /opt/airflow
+#docker exec -u root $WEBSERVER_ID chmod 777 /opt/airflow
+docker exec -u root $WEBSERVER_ID chown airflow.airflow /opt/airflow
 sleep 90
