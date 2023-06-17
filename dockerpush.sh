d4f298a673afdd82856c2baf24a57629d56311c6
Ebuk-a <cakwiwuuzoma@gmail.com>
2023-06-17 07:57:12 +0100

    cleanup repo

--- dockerpush.sh
+++ dockerpush.sh
@@ -10,12 +10,16 @@ WORKER_2_NAME=`docker ps | grep worker-2 | awk '{print $2}'`
 #Tag each local image according, ready for push to remote repo.
 docker tag $WEBSERVER_NAME:latest dataebuka/airflow-webserver:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully tagged $WEBSERVER_NAME:latest as dataebuka/airflow-webserver:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker tag $SCHEDULER_NAME:latest dataebuka/airflow-scheduler:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully tagged $SCHEDULER_NAME:latest as dataebuka/airflow-scheduler:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker tag $TRIGGERER_NAME:latest dataebuka/airflow-triggerer:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully tagged $TRIGGERER_NAME:latest as dataebuka/airflow-triggerer:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker tag $WORKER_1_NAME:latest dataebuka/airflow-worker-1:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully tagged $WORKER_1_NAME:latest as dataebuka/worker-1:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker tag $WORKER_2_NAME:latest dataebuka/airflow-worker-2:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully tagged $WORKER_2_NAME:latest as dataebuka/airflow-worker-2:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
 
@@ -25,12 +29,16 @@ docker login -u $DOCKER_CREDENTIAL_USR -p $DOCKER_CREDENTIAL_PSW
 #Push tagged images to remote repository
 docker push dataebuka/airflow-webserver:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully pushed dataebuka/airflow-webserver:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker push dataebuka/airflow-scheduler:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully pusheddataebuka/airflow-scheduler:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker push dataebuka/airflow-triggerer:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully pushed dataebuka/airflow-triggerer:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker push dataebuka/airflow-worker-1:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully pushed dataebuka/airflow-worker-1:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
+
 docker push dataebuka/airflow-worker-2:v$BUILD_NUMBER_$BUILD_TIMESTAMP
 echo "successfully pushed dataebuka/airflow-worker-2:v$BUILD_NUMBER_$BUILD_TIMESTAMP"
 
